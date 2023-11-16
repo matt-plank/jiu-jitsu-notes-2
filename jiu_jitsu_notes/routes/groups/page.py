28e6874a214e6aeec5e3ec8b1fa29439f2b7cd73
matt-plank <matthew_plank@outlook.com>
2023-11-16 16:52:42 +0000

    Changed position_group.html to group.html for clarity

--- jiu_jitsu_notes/routes/groups/page.py
+++ jiu_jitsu_notes/routes/groups/page.py
@@ -39,7 +39,7 @@ async def get_group(
         )
 
     return templates.TemplateResponse(
-        "pages/position_group.html",
+        "pages/group.html",
         {
             "request": request,
             "group": group,
