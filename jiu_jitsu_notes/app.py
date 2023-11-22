1e6cae058b3ec0eed37251b64abef8bb02ceb3c3
matt-plank <matthew_plank@outlook.com>
2023-11-22 22:13:05 +0000

    Added register route and template

--- jiu_jitsu_notes/app.py
+++ jiu_jitsu_notes/app.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 from .db import engine
 from .models import Base
-from .routes import groups, index, login, positions, techniques
+from .routes import groups, index, login, positions, register, techniques
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +14,7 @@ app.include_router(groups.router, prefix="/groups")
 app.include_router(positions.router, prefix="/positions")
 app.include_router(techniques.router, prefix="/positions")
 app.include_router(login.router, prefix="/login")
+app.include_router(register.router, prefix="/register")
 
 app.mount("/css", StaticFiles(directory="templates/css"), name="static")
 
